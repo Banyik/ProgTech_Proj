b877d29ee6ed8999568f30c584efc1ee465d7641
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2022-04-30 14:01:59 +0200

    Játékok feltöltésének elvégzése
    TODO:
    - ablak bezárása
    - előző ablak frissítése

--- ProgTech/AddNewToy.java
+++ ProgTech/AddNewToy.java
@@ -1,13 +1,23 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.nio.channels.SelectionKey;
+import java.sql.*;
 
 public class AddNewToy extends JFrame {
     private JTextField nameTField;
     private JTextField priceTField;
     private JLabel newtoyTitle;
     private JButton addBtn;
+
+    String[] nevek = {"Autó", "Plüss"};
     private JComboBox cbTypes;
     private JPanel addnewtoyPanel;
-
     public AddNewToy() {
         setContentPane(addnewtoyPanel);
         setTitle("Új játék felvitele");
@@ -15,7 +25,60 @@ public class AddNewToy extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
-        cbTypes = new JComboBox();
+        priceTField.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if ( ((c < '0') || (c > '9')) && (c != KeyEvent.VK_BACK_SPACE)) {
+                    e.consume();
+                }
+            }
+        });
+        addBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(validateTextFields()) {
+                    String ToyName = nameTField.getText();
+                    String SelectedType="Plush";
+                    if(String.valueOf(cbTypes.getSelectedItem()) == "Autó")
+                        SelectedType = "Car";
+                    String ToyPrice = priceTField.getText();
+
+
+                    String DB_URL = "jdbc:mysql://localhost:3306/jatekaruhaz";
+                    String USERNAME = "root";
+                    String PASSWORD = "";
+
+                    try{
+                        Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+
+                        Statement stmt = connection.createStatement();
+                        String sql = "INSERT INTO toy(name, type, price) VALUES('"+ToyName+"', '"+SelectedType+"', "+ToyPrice+")";
+                        stmt.execute(sql);
+                        stmt.close();
+
+                        stmt.close();
+                        connection.close();
+                    }
+                    catch(Exception ex) {
+                        ex.printStackTrace();
+                    }
+
+                }
+            }
+        });
+    }
+
+    private boolean validateTextFields() {
+        if (nameTField.getText().length() == 0) {
+            JOptionPane.showMessageDialog(null, "Adjon meg egy Megnevezést a temréknek!");
+            return false;
+        }
+
+        if (priceTField.getText().length() == 0) {
+            JOptionPane.showMessageDialog(null, "Adjon meg egy Egységárat a(z) Autónak!");
+            return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
