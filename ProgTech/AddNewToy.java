1c812805f7de15b5cb876015bb142a13df16ee91
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2022-04-30 13:12:53 +0200

    Játékok listázása Form

--- ProgTech/AddNewToy.java
+++ ProgTech/AddNewToy.java
@@ -1,14 +1,24 @@
 import javax.swing.*;
 
-public class AddNewToy {
+public class AddNewToy extends JFrame {
     private JTextField nameTField;
     private JTextField priceTField;
     private JLabel newtoyTitle;
     private JButton addBtn;
     private JComboBox cbTypes;
+    private JPanel addnewtoyPanel;
 
     public AddNewToy() {
-        setTitle("");
+        setContentPane(addnewtoyPanel);
+        setTitle("Új játék felvitele");
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        cbTypes = new JComboBox();
     }
 
+    public static void main(String[] args) {
+        AddNewToy addnewtoyForm = new AddNewToy();
+    }
 }
