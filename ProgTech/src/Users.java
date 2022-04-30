2005260b7e69a64db97686102669c923ad80d4f4
Banyik Nándor <83366829+Banyik@users.noreply.github.com>
2022-04-30 16:00:59 +0200

    Felhasználó modell feltöltése

---
+++ ProgTech/src/Users.java
@@ -0,0 +1,23 @@
+public class Users {
+    int id;
+    String username;
+    String auth;
+    String email;
+    String address;
+    public Users(int id, String username, String auth, String email, String address) {
+        this.id = id;
+        this.username = username;
+        this.auth = auth;
+        this.email = email;
+        this.address = address;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getAuth() {
+        return auth;
+    }
+}
