5e71d5a0f8943a4ac3f3be43d29a49b270a09ca2
KecseKaroly <83023819+KecseKaroly@users.noreply.github.com>
2022-04-30 19:06:07 +0200

    Játékok listázásának megvalósítása

--- ProgTech/src/ListToys.java
+++ ProgTech/src/ListToys.java
@@ -1,2 +1,83 @@
-public class ListToys {
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListToys extends JFrame {
+    private JPanel ListToysPanel;
+    private JButton button1;
+    private final ListToys ListToysForm = this;
+    private JTable table;
+    private final List<Toy> Toys = getToys();
+    public ListToys() throws SQLException {
+        setTitle("Játékok áruháza");
+        setSize(450, 300);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setVisible(true);
+        createTable();
+
+        table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent event) {
+                if (table.getSelectedRow() > -1) {
+
+                    Toy toy = Toys.get(table.getSelectedRow());
+                    Users user = new Users(2, "user", "user", "user@user.com", "User, User utca -1");
+                    ListToysForm.setEnabled(false);
+                    OrderToy OrderToy = new OrderToy(toy, user);
+                }
+            }
+        });
+    }
+
+    private List<Toy> getToys() throws SQLException {
+        String DB_URL = "jdbc:mysql://localhost:3306/jatekaruhaz";
+        String DB_USERNAME = "root";
+        String PASSWORD = "";
+        Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, PASSWORD);
+        Statement stmt = connection.createStatement();
+        String sql = "SELECT * FROM toy";
+        ResultSet result = stmt.executeQuery(sql);
+        List<Toy> toys = new ArrayList<>();
+        while (result.next()) {
+            Toy toy = new Toy(Integer.parseInt(result.getString("id")), result.getString("name"), Integer.parseInt(result.getString("id")));
+            toys.add(toy);
+        }
+        stmt.close();
+        connection.close();
+        return toys;
+    }
+    private void createTable() {
+        JFrame frame = this;
+        ListToysPanel = new JPanel();
+        String[][] ToysArr = new String[Toys.size()][3];
+        for (int i = 0; i < Toys.size()-1; i++) {
+            ToysArr[i][0] = String.valueOf(Toys.get(i).getId());
+            ToysArr[i][1] = String.valueOf(Toys.get(i).getName());
+            ToysArr[i][2] = String.valueOf(Toys.get(i).getPrice());
+        }
+        String[] header = { "Id", "Megnevezés", "Egységár" };
+        table = new JTable(ToysArr, header);
+        for (int c = 0; c < table.getColumnCount(); c++)
+        {
+            Class<?> col_class = table.getColumnClass(c);
+            table.setDefaultEditor(col_class, null);        // remove editor
+        }
+        table.getTableHeader().setReorderingAllowed(false);
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        ListToysPanel.add(new JScrollPane(table));
+        frame.add(ListToysPanel);
+        frame.setSize(550, 400);
+        frame.setVisible(true);
+        setContentPane(ListToysPanel);
+    }
+
+    public static void main(String[] args) throws SQLException {
+        new ListToys();
+    }
 }
--- ProgTech/src/OrderToy.java
+++ ProgTech/src/OrderToy.java
@@ -13,86 +13,74 @@ public class OrderToy extends JFrame {
     private JCheckBox cbCOD;
     private JCheckBox cbFragile;
     private JButton btnOrder;
+    private final JFrame frame;
     private JLabel prioDesc;
     private JLabel fragDesc;
     private JLabel codDesc;
-    private JTextField tfToyId;
-    private JTextField tfName;
-    private JTextField tfPrice;
     private JLabel lbId;
-    private JLabel lblName;
-    private JLabel lblPrice;
-    private JTextField tfUserId;
+    private final Toy toy;
+    private final Users user;
 
-    private void setComponents(Toy toy, Users user) {
-        tfToyId.setVisible(false);
-        tfUserId.setVisible(false);
+    private void setComponents() {
         setContentPane(OrderToyPanel);
         setTitle("Játék megrendelése");
         setSize(450, 300);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setVisible(true);
-        tfName.setText(toy.getName());
-        tfPrice.setText(Integer.toString(toy.getPrice()));
-        tfToyId.setText(Integer.toString(toy.getId()));
-        tfUserId.setText(user.getUsername());
     }
     public OrderToy(Toy toy, Users user) {
-        setComponents(toy, user);
+        setComponents();
+        this.toy = toy;
+        this.user = user;
+        this.frame = this;
         btnOrder.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Toy toy = CreateOrderedToy();
+                Toy decorated = CreateOrderedToy();
                 String DB_URL = "jdbc:mysql://localhost:3306/jatekaruhaz";
                 String DB_USERNAME = "root";
                 String PASSWORD = "";
-                String toyId = tfToyId.getText();
-                String userName = tfUserId.getText();
                 String address = tfSzallitasiCim.getText();
-                String price = Integer.toString(toy.getPrice());
                 try{
-                    InsertIntoOrder(DB_URL, DB_USERNAME, PASSWORD, toyId, userName, address, price);
-                    JOptionPane.showMessageDialog(null, "Sikeres rendelés!\nFizetendő összeg: "+toy.getPrice()+" Ft.");
-                    dispose();
+                    InsertIntoOrder(DB_URL, DB_USERNAME, PASSWORD, decorated, user.getUsername(), address);
+                    JOptionPane.showMessageDialog(null, "Sikeres rendelés!\nFizetendő összeg: "+decorated.getPrice()+" Ft.");
                 }
                 catch(Exception ex) {
                     ex.printStackTrace();
                 }
             }
-
             private void InsertIntoOrder(String DB_URL,
                                          String USERNAME,
                                          String PASSWORD,
-                                         String toyId,
+                                         Toy decorated,
                                          String userName,
-                                         String address,
-                                         String price) throws SQLException {
+                                         String address) throws SQLException {
                 Connection connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
                 Statement stmt = connection.createStatement();
-                String sql = "INSERT INTO `order` ( `object_id`, `addressee`, `mailing address`, `price`) VALUES ('"+toyId+"', '"+userName+"', '"+address+"', '"+price+"')";
+                String sql = "INSERT INTO `order` ( `object_id`, `addressee`, `mailing address`, `price`) VALUES ('"+decorated.getId()+"', '"+userName+"', '"+address+"', '"+decorated.getPrice()+"')";
                 stmt.execute(sql);
                 stmt.close();
                 connection.close();
             }
 
             private Toy CreateOrderedToy() {
-                Toy toy = new Toy(Integer.parseInt(tfToyId.getText()), tfName.getText(), Integer.parseInt(tfPrice.getText()));
+                Toy decorated = new Toy(toy.getId(), toy.getName(), toy.getPrice());
                 if(cbCOD.isSelected()) {
-                    toy = new CodToyDecorator(toy);
+                    decorated = new CodToyDecorator(decorated);
                 }
                 if(cbFragile.isSelected()) {
-                    toy = new FragileToyDecorator(toy);
+                    decorated = new FragileToyDecorator(decorated);
                 }
                 if(cbPriority.isSelected()) {
-                    toy = new PriorityToyDecorator(toy);
+                    decorated = new PriorityToyDecorator(decorated);
                 }
-                return toy;
+                return decorated;
             }
         });
     }
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Toy jatek = new Toy(1, "Pelda nev", 123);
         Users user = new Users(2, "user", "user", "user@user.com", "User, User utca -1");
         OrderToy OrderToyForm = new OrderToy(jatek, user);
-    }
+    }*/
 }
